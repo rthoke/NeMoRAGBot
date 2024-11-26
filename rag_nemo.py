5bf2ed7aa096ed039555ea0cf03d0eeca58e5666
rthoke <121438400+rthoke@users.noreply.github.com>
2024-11-26 14:17:40 +0530

    Update rag_nemo.py

--- rag_nemo.py
+++ rag_nemo.py
@@ -6,10 +6,9 @@ from langchain_chroma import Chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings
-sys.path.append("../NeMo-Guardrails")
 from nemoguardrails import RailsConfig, LLMRails
 os.environ["NVIDIA_API_KEY"] = "APIKEY"
-embeddings = NVIDIAEmbeddings(base_url="http://192.168.100.64:2000/v1",model='nvidia/nv-embedqa-e5-v5')
+embeddings = NVIDIAEmbeddings(model='nvidia/nv-embedqa-e5-v5')
 vect = Chroma(persist_directory=persist_directory, embedding_function=embeddings,collection_name=collection_name)
 CNretriever = vect.as_retriever()
 config = RailsConfig.from_path("config")
